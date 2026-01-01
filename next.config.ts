ba73d68ae942e99b6743d62cd47b010f38d8acc2
MindSpan Developer <mindspan@local.dev>
2026-01-01 23:46:23 +0530

    Build stable on Vercel free (Option B)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+  typedRoutes: false
 };
 
 export default nextConfig;
